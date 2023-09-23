53d8942929983c0f5cb76eae18753a02535f5df8
Nate Rathbun <naterathbun@gmail.com>
2023-09-23 14:02:03 -0500

    Added logic for Prime Number Module

--- InterviewReviewer/Modules/PrimeNumberChecker.cs
+++ InterviewReviewer/Modules/PrimeNumberChecker.cs
@@ -14,11 +14,33 @@ namespace InterviewReviewer.Modules
 
         public void Run()
         {
-            
+            Console.Write("Please enter a positive integer: ");
+            var userInputNumber = Console.ReadLine() ?? "0";
+            var numberToCheck = int.Parse(userInputNumber);
 
+            if (IsPrimeNumber(numberToCheck))
+                Console.WriteLine("\nYes, your number, {0}, is a prime number.", numberToCheck);
+            else
+                Console.WriteLine("\nNo, your number, {0}, isn't a prime number", numberToCheck);
+        }
 
+        private bool IsPrimeNumber(int numberToCheck)
+        {
+            // Check special cases (Negatives, 0, 1, 2, 3) and numbers divisable by 2 or 3
+            if (numberToCheck <= 1 || numberToCheck % 2 == 0 || numberToCheck % 3 == 0)
+                return false;
+            if (numberToCheck == 2 || numberToCheck == 3)
+                return true;
 
+            // Check for factors using the 6k+/-1 rule
+            for (int i = 5; i * i <= numberToCheck; i += 6)
+            {
+                if (numberToCheck % i == 0 || numberToCheck % (i + 2) == 0)
+                    return false;
+            }
 
+            // Everything else is prime
+            return true;
         }
     }
 }
